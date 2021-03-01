38cb3394d8d0aa192bd121632830a550aa17c099
Avery Burns <a.aburns95@gmail.com>
2021-02-28 23:48:49 -0600

    creating routes

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,18 +1,41 @@
 import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom';
-import Login from './Login.js';
-import Register from './Register.js';
+import PrivateRoute from "./PrivateRoute";
+import Login from './Components/Login.js';
+import Register from './Components/Register.js';
 import "./App.css";
 import CreateEvent from "./Components/CreateEvent";
 
 
 function App() {
   return (
-    <div className="App">
-      <Register/>
-      <Login/>
-      <CreateEvent /> 
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <div className="App-logo">
+            <h1>Fitness Tracker</h1>
+          </div>
+          <ul className="nav" >
+            <li>
+              <Link to="/"> Login</Link>
+            </li>
+            <li>
+              <Link to="/home">Home</Link>
+            </li>
+          </ul>
+        </header>
+        <section className="main-content">
+          <Switch>
+            <Route exact path="/" component={Login} />
+            <Route exact path="/home" component={Register} />
+          </Switch>
+        </section>
+        <Register/>
+      
+      {/* <Login/>
+      <CreateEvent />  */}
     </div>
+    </Router>
   );
 }
 
